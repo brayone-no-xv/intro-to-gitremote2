c70ad2e93ed9ff1cd3a7cd3cf6a6cbbc91f91eaa
hellostudent <hrepunzel@gmail.com>
2024-09-27 13:25:30 +0700

    adding list

--- index.php
+++ index.php
@@ -8,5 +8,8 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi blanditiis labore, facere ipsum quia perferendis, eos, similique quod maiores repellat sunt vel doloribus. Nulla fugiat reprehenderit accusamus, deserunt magnam et?</p>
+    <li>
+        <ul>list</ul>
+    </li>
 </body>
 </html>
